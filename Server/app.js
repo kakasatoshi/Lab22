afdd97011ad3ea0dcf832f52ba05fa0c34189b11
kakasatoshi <84920014+kakasatoshi@users.noreply.github.com>
2025-02-11 21:41:42 +0700

    dsds

--- Server/app.js
+++ Server/app.js
@@ -1,32 +1,32 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const graphqlHttp = require('express-graphql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const { graphqlHTTP } = require("express-graphql");
 
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
-const auth = require('./middleware/auth');
-const { clearImage } = require('./util/file');
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
+const { clearImage } = require("./util/file");
 
 const app = express();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -37,18 +37,18 @@ const fileFilter = (req, file, cb) => {
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
@@ -56,24 +56,25 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
-app.put('/post-image', (req, res, next) => {
+app.put("/post-image", (req, res, next) => {
   if (!req.isAuth) {
-    throw new Error('Not authenticated!');
+    throw new Error("Not authenticated!");
   }
   if (!req.file) {
-    return res.status(200).json({ message: 'No file provided!' });
+    return res.status(200).json({ message: "No file provided!" });
   }
   if (req.body.oldPath) {
     clearImage(req.body.oldPath);
   }
   return res
     .status(201)
-    .json({ message: 'File stored.', filePath: req.file.path });
+    .json({ message: "File stored.", filePath: req.file.path });
 });
 
 app.use(
-  '/graphql',
-  graphqlHttp({
+  "/graphql",
+  graphqlHTTP({
+    // <- Sửa chữ H hoa
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
@@ -82,10 +83,10 @@ app.use(
         return err;
       }
       const data = err.originalError.data;
-      const message = err.message || 'An error occurred.';
+      const message = err.message || "An error occurred.";
       const code = err.originalError.code || 500;
       return { message: message, status: code, data: data };
-    }
+    },
   })
 );
 
@@ -99,11 +100,10 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/messages?retryWrites=true'
+    "mongodb+srv://kakasatoshi:Mnbv%400987@product.6wlp4.mongodb.net/Product2?retryWrites=true&w=majority&appName=Product2",
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
-  .then(result => {
+  .then((result) => {
     app.listen(8080);
   })
-  .catch(err => console.log(err));
-
-
+  .catch((err) => console.log(err));
